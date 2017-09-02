e425e0de0020d439e4a9ebb05495b8666a4e21ce
Jacob Goh <jacobgoh101@gmail.com>
2017-09-02 11:40:44 +0800

    1st

---
+++ js/app.js
@@ -0,0 +1,52 @@
+import '../css/app.scss';
+const feathers = require('feathers/client');
+const rest = require('feathers-rest/client');
+const axios = require('axios');
+const hooks = require('feathers-hooks');
+const localStorage = require('localstorage-memory');
+const auth = require('feathers-authentication-client');
+const hello = require('hellojs/dist/hello.all.js');
+
+const client = feathers();
+// NOTE: the order is important: auth must be configured _after_ rest/socket
+client
+    .configure(hooks())
+    .configure(rest('http://localhost:3030').axios(axios))
+    .configure(auth({storage: localStorage}));
+
+// START setup hellojs
+hello.on('auth.login', async function (auth) {
+    const socialToken = auth.authResponse.access_token;
+    const userInfo = await hello(auth.network).api('me');
+    const userId = userInfo.id;
+    const userEmail = userInfo.email;
+    serverAuth(userEmail, userId, socialToken);
+});
+hello.init({
+    google: '28065581468-4rbjg22ltb9n6nd183e9mh92rsuqe3j7.apps.googleusercontent.com'
+}, {redirect_uri: '/index.html'});
+document
+    .querySelector('.btn-google')
+    .addEventListener('click', () => {
+        hello('google').login({scope: 'email'})
+    })
+// END setup hellojs
+
+async function serverAuth(email, socialId, socialToken) {
+    try {
+        const response = await client.authenticate({strategy: 'social-token', email, socialId, socialToken});
+        console.log('Authenticated!', response);
+        const payload = await client
+            .passport
+            .verifyJWT(response.accessToken);
+        console.log('JWT Payload', payload);
+        const user = await client
+            .service('users')
+            .get(payload.userId);
+        client.set('user', user);
+        console.log('User', client.get('user'));
+    } catch (err) {
+        console.error('Error authenticating!');
+        console.error(err);
+    }
+}
